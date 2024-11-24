ea23641272427d4b6eed6671d27da72a30d314a6
Afiakyalo <afiakyalo@gmail.com>
2024-11-23 19:21:30 -0500

    add tests, fix ci run issue, remove rubocop run for now

--- test/controllers/affirmations_controller_test.rb
+++ test/controllers/affirmations_controller_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
 class AffirmationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get affirmations_index_url
-    assert_response :success
+  setup do
+    @affirmation = affirmations(:one) # Assuming you have a fixture named :one
   end
 
-  test "should get next" do
-    get affirmations_next_url
+  test "should get index" do
+    get affirmations_url
     assert_response :success
+    assert_select '#affirmation p', @affirmation.content
+  end
+
+  test "should get next affirmation" do
+    get next_affirmation_url
+    assert_response :success
+    json_response = JSON.parse(@response.body)
+    assert json_response['content'].present?
   end
 end
